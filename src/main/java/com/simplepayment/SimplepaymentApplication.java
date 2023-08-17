481b140f72d03b697e0c2d6b3b1624279ef05dfb
FIRMINOenzo <enzofirmino.camp@gmail.com>
2023-08-16 21:44:06 -0300

    feat: initial commit

---
+++ src/main/java/com/simplepayment/SimplepaymentApplication.java
@@ -0,0 +1,13 @@
+package com.simplepayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplepaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplepaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplepayment/SimplepaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplepayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplepaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
