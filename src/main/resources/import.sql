c1e26785549a4b5dec72c184554c1824175876c8
FIRMINOenzo <enzofirmino.camp@gmail.com>
2023-08-21 19:58:32 -0300

    feat: adding Transfer Repository & seeding DB

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,5 @@
 INSERT INTO user_types(typ_description) VALUES('Common'), ('Merchandiser');
+
+INSERT INTO users(use_first_name, use_last_name, use_email, use_password, use_document, use_balance, use_type_id) VALUES('Enzo', 'Campanari', 'test@test.com', '1234', '12312312312', 150.50, 1), ('Pedro', 'Cavalo', 'test2@test.com', '123456', '12345678909', 130.50, 2);
+
+INSERT INTO transfers(tra_sender_id, tra_amount, tra_receiver_id) VALUES(1, 12.30, 2);
