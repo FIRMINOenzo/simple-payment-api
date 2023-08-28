2e51a662a41f64a7b6ae1aeeb3a2b005e096e8e2
Enzo Firmino Campanari <114703471+FIRMINOenzo@users.noreply.github.com>
2023-08-28 23:33:23 +0000

    refactor: removing unused imports

--- src/main/java/com/simplepayment/controllers/TransferController.java
+++ src/main/java/com/simplepayment/controllers/TransferController.java
@@ -1,9 +1,6 @@
 package com.simplepayment.controllers;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
