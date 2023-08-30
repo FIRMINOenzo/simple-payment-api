646f82860eca21cf2e7ad082f12da5d574515912
FIRMINOenzo <enzofirmino.camp@gmail.com>
2023-08-30 18:10:48 -0300

    feat: adding User Projection

---
+++ src/main/java/com/simplepayment/projections/UserProjection.java
@@ -0,0 +1,11 @@
+package com.simplepayment.projections;
+
+public interface UserProjection {
+    Long getUserId();
+
+    String getUserFirstName();
+
+    String getUserLastName();
+
+    String getUserType();
+}
