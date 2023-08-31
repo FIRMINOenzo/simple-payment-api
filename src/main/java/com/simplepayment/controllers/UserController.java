d51717d2b66c188352865aa58a272ce56fafbc33
FIRMINOenzo <enzofirmino.camp@gmail.com>
2023-08-30 21:32:00 -0300

    feat: adding GET user by ID endpoint

--- src/main/java/com/simplepayment/controllers/UserController.java
+++ src/main/java/com/simplepayment/controllers/UserController.java
@@ -1,9 +1,13 @@
 package com.simplepayment.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.simplepayment.dtos.UserDTO;
 import com.simplepayment.services.UserService;
 
 @RestController
@@ -12,4 +16,15 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
+        UserDTO user = userService.getUserInfoById(id);
+
+        if (user != null) {
+            return ResponseEntity.ok(user);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
 }
--- src/main/java/com/simplepayment/services/UserService.java
+++ src/main/java/com/simplepayment/services/UserService.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.simplepayment.dtos.UserDTO;
 import com.simplepayment.entities.User;
+import com.simplepayment.projections.UserProjection;
 import com.simplepayment.repositories.UserRepository;
 
 @Service
@@ -19,6 +21,22 @@ public class UserService {
 
         if (user.isPresent()) {
             return user.get();
+        }
+
+        return null;
+    }
+
+    public UserDTO getUserInfoById(Long id) {
+        Optional<UserProjection> user = userRepository.getUserInfoById(id);
+
+        if (user.isPresent()) {
+
+            UserDTO response = new UserDTO(user.get());
+
+            System.out.println(response.getFirstName());
+            System.out.println(response.getLastName());
+
+            return response;
         }
 
         return null;
