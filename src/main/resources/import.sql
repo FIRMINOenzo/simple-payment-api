054f460cd5a883240f2387bfd172c1912dbf814b
Enzo Firmino Campanari <114703471+FIRMINOenzo@users.noreply.github.com>
2023-09-01 22:51:57 +0000

    feat: adding new INSERTS to test

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,3 +3,8 @@ INSERT INTO user_types(typ_description) VALUES('Common'), ('Merchandiser');
 INSERT INTO users(use_first_name, use_last_name, use_email, use_password, use_document, use_balance, use_type_id) VALUES('Enzo', 'Campanari', 'test@test.com', '1234', '12312312312', 150.50, 1), ('Pedro', 'Cavalo', 'test2@test.com', '123456', '12345678909', 130.50, 2);
 
 INSERT INTO transfers(tra_sender_id, tra_amount, tra_receiver_id) VALUES(1, 12.30, 2);
+INSERT INTO transfers(tra_sender_id, tra_amount, tra_receiver_id) VALUES(1, 1.30, 2);
+INSERT INTO transfers(tra_sender_id, tra_amount, tra_receiver_id) VALUES(1, 2.30, 2);
+INSERT INTO transfers(tra_sender_id, tra_amount, tra_receiver_id) VALUES(1, 12.0, 2);
+INSERT INTO transfers(tra_sender_id, tra_amount, tra_receiver_id) VALUES(1, 122.30, 2);
+INSERT INTO transfers(tra_sender_id, tra_amount, tra_receiver_id) VALUES(1, 1.60, 2);
